ac6ed370e171ed3fdc5fa0a373536ce8aefe9081
Baluku Brian <43814291+BalukuBrian@users.noreply.github.com>
2021-03-05 15:51:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/balbripcu/ViewCourseUnits.java
@@ -0,0 +1,30 @@
+package com.example.balbripcu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ViewCourseUnits extends AppCompatActivity {
+
+      private Button backBtn ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_print_course_units);
+
+        backBtn = findViewById(R.id.back_btn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+
+    }
+}
