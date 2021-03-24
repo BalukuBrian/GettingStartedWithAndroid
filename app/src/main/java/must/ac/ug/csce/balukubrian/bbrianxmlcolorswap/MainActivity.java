c25375bd6c98a21cb93e1f6656d6ff6aec32d8e6
Baluku Brian <43814291+BalukuBrian@users.noreply.github.com>
2021-03-24 16:38:51 +0300

    initial commit

--- app/src/main/java/must/ac/ug/csce/balukubrian/bbrianxmlcolorswap/MainActivity.java
+++ app/src/main/java/must/ac/ug/csce/balukubrian/bbrianxmlcolorswap/MainActivity.java
@@ -3,6 +3,11 @@ package must.ac.ug.csce.balukubrian.bbrianxmlcolorswap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        final Button tapMebutton = findViewById(R.id.tap_me_button);
+        final TextView tvChangeColor = findViewById(R.id.tvChangeColor);
+
+        tapMebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+    });
+}
 }
