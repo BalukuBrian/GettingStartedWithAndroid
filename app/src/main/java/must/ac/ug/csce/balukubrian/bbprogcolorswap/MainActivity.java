9ea7beb07ca214d835b258333e27df777d576802
Baluku Brian <43814291+BalukuBrian@users.noreply.github.com>
2021-03-24 15:46:29 +0300

    added a randomColor method in the MainActivity

--- app/src/main/java/must/ac/ug/csce/balukubrian/bbprogcolorswap/MainActivity.java
+++ app/src/main/java/must/ac/ug/csce/balukubrian/bbprogcolorswap/MainActivity.java
@@ -16,22 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int[] colorForRandom;
-
-        //in the onCreate
-        colorForRandom = new int[] {
-                (R.color.black),
-                (R.color.red),
-                (R.color.yellow),
-                (R.color.green),
-                (R.color.purple_700),
-                (R.color.teal_200),
-                (R.color.teal_700),
-        };
+//
+//        int[] colorForRandom;
+//
+//        //in the onCreate
+//        colorForRandom = new int[] {
+//                (R.color.black),
+//                (R.color.red),
+//                (R.color.yellow),
+//                (R.color.green),
+//                (R.color.purple_700),
+//                (R.color.teal_200),
+//                (R.color.teal_700),
+//        };
 
         final Button tapMebutton = findViewById(R.id.tap_me_button);
-        final TextView textview_Tap_to_Change_Color = findViewById(R.id.textview_Tap_to_Change_Color);
+        final TextView tvChangeColor = findViewById(R.id.tvChangeColor);
 
         tapMebutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,20 +42,34 @@ public class MainActivity extends AppCompatActivity {
 //                textview_Tap_to_Change_Color.setBackgroundColor(getResources().getColor(R.color.green));
 //                textview_Tap_to_Change_Color.setBackgroundColor(getResources().getColor(R.color.teal_200));
 //                textview_Tap_to_Change_Color.setBackgroundColor(getResources().getColor(R.color.purple_700));
+//
+//                int arrayLength;
+//                arrayLength = colorForRandom.length;
+//
+//                Random randomColor = new Random();
+//                int rNum = randomColor.nextInt(arrayLength);
+//
+//                tvChangeColor.setTextColor(colorForRandom[rNum]);
 
-                int arrayLength;
-                arrayLength = colorForRandom.length;
-
-                Random randomColor = new Random();
-                int rNum = randomColor.nextInt(arrayLength);
-
-                textview_Tap_to_Change_Color.setTextColor(colorForRandom[rNum]);
-
+                tvChangeColor.setTextColor(randomColor());
+                //tvChangeColor.setText("COLOR: "+ randomColor(r)+"r"+ randomColor(g)+"g"+ randomColor(b)+"b");
 
 
             }
         });
 
 
+    }
+
+    /** The method randomColor uses java Random class to generate
+     * random value between 0 to 255 for red, blue and green values.
+     */
+
+    private int randomColor() {
+        Random random = new Random();
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+        return Color.rgb(r, g, b);
     }
 }
